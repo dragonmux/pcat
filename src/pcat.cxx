cb2ea576198e8ced5b27513977d61743aa41e9f5
dx-mon <dx-mon@users.sourceforge.net>
2020-04-15 10:27:38 +0100

    Added various key includes and defined the size of a page

--- src/pcat.cxx
+++ src/pcat.cxx
@@ -1,4 +1,12 @@
+#include <cstring>
 #include <substrate/fd>
+#include <substrate/mmap>
+#include <substrate/units>
+
+using substrate::mmap_t;
+using substrate::operator ""_KiB;
+
+constexpr static size_t pageSize = 4_KiB;
 
 int main(int argCount, char **argList)
 {
