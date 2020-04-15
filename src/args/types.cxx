d1eb9153496315f34a1bdcad745f738c9c1f842a
dx-mon <dx-mon@users.sourceforge.net>
2020-04-15 15:02:26 +0100

    Made the operation of the defintions in args/types.cxx easier to understand

--- src/args/types.cxx
+++ src/args/types.cxx
@@ -1,32 +1,33 @@
 #include "../args.hxx"
 
-using namespace pcat::args;
-
-argNode_t *argsTree_t::find(const argType_t nodeType, const bool recursive) const noexcept
+namespace pcat::args
 {
-	for (const auto &node : children_)
-	{
-		if (node->type() == nodeType)
-			return node.get();
-	}
-
-	if (recursive)
+	argNode_t *argsTree_t::find(const argType_t nodeType, const bool recursive) const noexcept
 	{
 		for (const auto &node : children_)
 		{
-			if (node->type() == argType_t::tree)
+			if (node->type() == nodeType)
+				return node.get();
+		}
+
+		if (recursive)
+		{
+			for (const auto &node : children_)
 			{
-				auto tree = static_cast<const argsTree_t *>(node.get());
-				auto result = tree->find(nodeType, true);
-				if (result)
-					return result;
+				if (node->type() == argType_t::tree)
+				{
+					auto tree = static_cast<const argsTree_t *>(node.get());
+					auto result = tree->find(nodeType, true);
+					if (result)
+						return result;
+				}
 			}
 		}
+		return nullptr;
 	}
-	return nullptr;
-}
 
-bool argsTree_t::add(std::unique_ptr<argNode_t> &&node) noexcept try
-	{ return bool{children_.emplace_back(std::move(node))}; }
-catch (std::bad_alloc &)
-	{ return false; }
+	bool argsTree_t::add(std::unique_ptr<argNode_t> &&node) noexcept try
+		{ return bool{children_.emplace_back(std::move(node))}; }
+	catch (std::bad_alloc &)
+		{ return false; }
+}
