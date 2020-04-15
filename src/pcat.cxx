0a2db030f3011a00e8a943717307df779ecbd7b3
dx-mon <dx-mon@users.sourceforge.net>
2020-04-15 18:16:34 +0100

    Improved the logic for gathering files to concatenate and summing their total size

--- src/pcat.cxx
+++ src/pcat.cxx
@@ -44,36 +44,36 @@ namespace pcat
 	bool checkFile(const std::string_view fileName) noexcept
 	{
 		fd_t file{fileName.data(), O_RDONLY | O_NOCTTY};
-		if (!file.valid())
+		// If the file wasn't able to be opened, or is not stat()-able, discard it.
+		if (!file.valid() || file.length() <= 0)
 			return false;
+		// Change it from just open, to exclusive access so it can't be removed or changed from under us.
+		//
 		inputFiles.emplace_back(std::move(file));
 		return true;
 	}
 
 	bool gatherFiles() noexcept
 	{
-		bool result{true};
 		for (const auto &arg : *::args)
 		{
 			if (arg->type() != argType_t::unrecognised)
 				continue;
 			const auto &file{dynamic_cast<args::argUnrecognised_t &>(*arg)};
-			result &= checkFile(file.argument());
+			if (!checkFile(file.argument()))
+				return false;
 		}
-		return result && !inputFiles.empty();
+		return true;
 	}
 
 	std::size_t totalSize() noexcept
 	{
 		return std::accumulate(inputFiles.begin(), inputFiles.end(), 0,
 			[](const std::size_t count, const fd_t &file) noexcept -> std::size_t
-			{
-				const auto length = file.length();
-				return length > 0 ? count + length : count;
-			}
+				{ return count + file.length(); }
 		);
 	}
-}
+} // namespace pcat
 
 int main(int argCount, char **argList)
 {
@@ -95,7 +95,7 @@ int main(int argCount, char **argList)
 	dumpAST();
 	if (!pcat::gatherFiles())
 	{
-		console.error("Cannot find any valid files to concatentate, exiting."sv); // NOLINT(readability-magic-numbers)
+		console.error("One or more files specified to concatentate are invalid, exiting."sv); // NOLINT(readability-magic-numbers)
 		return 1;
 	}
 	return 0;
