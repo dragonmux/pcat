5a30f3a0afcc70cabaaef2653f67bd5cb8ac3a2a
dx-mon <dx-mon@users.sourceforge.net>
2020-04-15 15:52:47 +0100

    Added handling for --help/-h

---
+++ src/help.hxx
@@ -0,0 +1,13 @@
+#ifndef HELP__HXX
+#define HELP__HXX
+
+using std::literals::string_view_literals::operator ""sv;
+
+namespace pcat
+{
+	constexpr static auto helpString{
+		""sv
+	};
+}
+
+#endif /*HELP__HXX*/
--- src/pcat.cxx
+++ src/pcat.cxx
@@ -1,6 +1,9 @@
+#include <cstdint>
+#include <cstdio>
 #include <cstring>
-#include <array>
 #include <string_view>
+#include <array>
+#include <vector>
 #include <substrate/fd>
 #include <substrate/mmap>
 #include <substrate/utility>
@@ -8,6 +11,7 @@
 #include <substrate/console>
 #include <version.hxx>
 #include "args.hxx"
+#include "help.hxx"
 
 using std::literals::string_view_literals::operator ""sv;
 using substrate::console;
@@ -21,11 +25,18 @@ namespace pcat
 	{
 		{"--version"sv, argType_t::version},
 		{"--help"sv, argType_t::help},
+		{"-h"sv, argType_t::help},
 		{"--output"sv, argType_t::outputFile},
 		{"-o"sv, argType_t::outputFile}
 	})};
 
 	constexpr static size_t pageSize = 4_KiB;
+
+	inline int32_t printHelp()
+	{
+		puts(helpString.data());
+		return 0;
+	}
 }
 
 int main(int argCount, char **argList)
@@ -36,8 +47,15 @@ int main(int argCount, char **argList)
 		console.error("Failed to parse arguments"sv); // NOLINT(readability-magic-numbers)
 		return 1;
 	}
+	if (args->find(argType_t::version) && args->find(argType_t::help))
+	{
+		console.error("Can only specify one of --help and --version, not both.");
+		return 1;
+	}
 	if (args->find(argType_t::version))
 		return pcat::versionInfo::printVersion();
+	else if (args->find(argType_t::help))
+		return pcat::printHelp();
 	dumpAST();
 	return 0;
 }
