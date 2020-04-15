019f5b0406462dd54959f159f9f179e19833623d
dx-mon <dx-mon@users.sourceforge.net>
2020-04-15 19:36:44 +0100

    Begun playing with different file locking strategies to protect the input file data from being munged part way through a concatenation operation

--- src/pcat.cxx
+++ src/pcat.cxx
@@ -10,6 +10,7 @@
 #include <substrate/utility>
 #include <substrate/units>
 #include <substrate/console>
+#include <sys/file.h>
 #include <version.hxx>
 #include "args.hxx"
 #include "help.hxx"
@@ -41,14 +42,39 @@ namespace pcat
 		return 0;
 	}
 
+	bool lockFile(const fd_t &file) noexcept
+	{
+		struct flock lock{};
+		lock.l_type = F_WRLCK;
+		lock.l_whence = SEEK_SET;
+		// This combo means "whole file, for eternity, even if it grows".
+		lock.l_start = 0;
+		lock.l_len = 0;
+		return fcntl(file, F_SETLK, &lock) == 0 && // NOLINT(cppcoreguidelines-pro-type-vararg)
+			flock(file, LOCK_NB | LOCK_MAND | LOCK_READ) == 0;
+	}
+
+	bool unlockFile(const fd_t &file) noexcept
+	{
+		struct flock lock{};
+		lock.l_type = F_UNLCK;
+		lock.l_whence = SEEK_SET;
+		// This combo means "whole file, for eternity, even if it grows".
+		lock.l_start = 0;
+		lock.l_len = 0;
+		return fcntl(file, F_SETLK, &lock) == 0 && // NOLINT(cppcoreguidelines-pro-type-vararg)
+			flock(file, LOCK_UN) == 0;
+	}
+
 	bool checkFile(const std::string_view fileName) noexcept
 	{
-		fd_t file{fileName.data(), O_RDONLY | O_NOCTTY};
+		fd_t file{fileName.data(), O_RDWR | O_NOCTTY};
 		// If the file wasn't able to be opened, or is not stat()-able, discard it.
 		if (!file.valid() || file.length() <= 0)
 			return false;
 		// Change it from just open, to exclusive access so it can't be removed or changed from under us.
-		//
+		if (!lockFile(file))
+			return false;
 		inputFiles.emplace_back(std::move(file));
 		return true;
 	}
@@ -72,6 +98,13 @@ namespace pcat
 			[](const std::size_t count, const fd_t &file) noexcept -> std::size_t
 				{ return count + file.length(); }
 		);
+	}
+
+	void closeFiles() noexcept
+	{
+		for (const auto &file : inputFiles)
+			unlockFile(file);
+		inputFiles.clear();
 	}
 } // namespace pcat
 
@@ -98,5 +131,8 @@ int main(int argCount, char **argList)
 		console.error("One or more files specified to concatentate are invalid, exiting."sv); // NOLINT(readability-magic-numbers)
 		return 1;
 	}
+	console.info("Length of the resulting concatenated file will be "sv, pcat::totalSize(), " bytes"sv);
+
+	pcat::closeFiles();
 	return 0;
 }
