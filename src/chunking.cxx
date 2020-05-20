20713d88c98e0ce60512be4d7f51967ff88dab27
dx-mon <dx-mon@users.sourceforge.net>
2020-05-20 19:11:31 +0100

    Removed the now defunct calculateInputChunking()

--- src/chunking.cxx
+++ src/chunking.cxx
@@ -135,55 +135,6 @@ namespace pcat
 		chunking_t end() noexcept { return {inputFiles.end()}; }
 	};
 
-	void calculateInputChunking() noexcept
-	{
-		auto file = inputFiles.begin();
-		off_t inputLength = file->length();
-		mappingOffset_t inputOffset{0, blockLength(inputLength)};
-
-		const off_t outputLength = outputFile.length();
-		for (mappingOffset_t outputOffset{}; outputOffset < outputLength; outputOffset += transferBlockSize)
-		{
-			outputOffset.length(blockLength(outputLength - outputOffset));
-			console.info("Copying ", inputOffset.length(), " bytes at ", inputOffset.offset(),
-				" to ", outputOffset.length(), " byte region at ", outputOffset.offset());
-
-			const auto nextInputBlock = [&]() -> void
-			{
-				inputOffset += inputOffset.length();
-				if (inputOffset.offset() == inputLength)
-				{
-					++file;
-					assert(file <= inputFiles.end()); // NOLINT
-					inputLength = file == inputFiles.end() ? 0 : file->length();
-					inputOffset = {};
-				}
-			};
-
-			if (outputOffset.length() != inputOffset.length())
-			{
-				const off_t originalOutputOffset = outputOffset.offset();
-				while (outputOffset.length() - inputOffset.length())
-				{
-					const off_t remainder = outputOffset.length() - inputOffset.length();
-					console.info("Transfer caused a remainder of ", remainder, " bytes to go for output block");
-					outputOffset += inputOffset.length();
-					outputOffset.length(remainder);
-					nextInputBlock();
-					inputOffset.length(std::min(remainder, inputLength));
-
-					console.info("Copying ", inputOffset.length(), " bytes at ", inputOffset.offset(),
-						" to ", outputOffset.length(), " byte region at ", outputOffset.offset());
-				}
-				outputOffset = {originalOutputOffset};
-			}
-			nextInputBlock();
-			inputOffset.length(blockLength(inputLength - inputOffset));
-		}
-
-		assert(file == inputFiles.end()); // NOLINT
-	}
-
 	int32_t chunkedCopy() noexcept
 	{
 		fileChunker_t chunker{};
