a31786330c8d0a8a643d273a37d0bf8a5d6a6a6d
dx-mon <dx-mon@users.sourceforge.net>
2020-06-08 18:27:35 +0100

    Fixed up a pile of clang-tidy warnings in the mmap_t header

--- src/mmap.hxx
+++ src/mmap.hxx
@@ -13,10 +13,10 @@ namespace pcat
 	struct mmap_t final
 	{
 	private:
-		off_t _len;
-		void *_addr;
+		off_t _len{0};
+		void *_addr{nullptr};
 
-		void *index(const off_t idx) const
+		[[nodiscard]] void *index(const off_t idx) const
 		{
 			if (idx < _len)
 			{
@@ -31,18 +31,20 @@ namespace pcat
 		using fd_t = substrate::fd_t;
 
 	public:
-		constexpr mmap_t() noexcept : _len{0}, _addr{nullptr} { }
+		constexpr mmap_t() noexcept = default;
 		mmap_t(const fd_t &fd, const off_t len, const int32_t prot, const int32_t flags = MAP_SHARED,
 			void *addr = nullptr) noexcept : _len{len}, _addr{[&]() noexcept -> void *
 			{
-				const auto ptr = ::mmap(addr, len, prot, flags, fd, 0);
+				auto *const ptr = ::mmap(addr, len, prot, flags, fd, 0);
+				// NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast)
 				return ptr == MAP_FAILED ? nullptr : ptr;
 			}()} { }
 		mmap_t(const fd_t &fd, const off_t offset, const off_t length, const int32_t prot,
 			const int32_t flags = MAP_SHARED, void *addr = nullptr) noexcept : _len{length},
 			_addr{[&]() noexcept -> void *
 			{
-				const auto ptr = ::mmap(addr, length, prot, flags, fd, offset);
+				auto *const ptr = ::mmap(addr, length, prot, flags, fd, offset);
+				// NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast)
 				return ptr == MAP_FAILED ? nullptr : ptr;
 			}()} { }
 		mmap_t(const mmap_t &) = delete;
@@ -65,8 +67,8 @@ namespace pcat
 		}
 
 		[[nodiscard]] constexpr off_t length() const noexcept { return _len; }
-		[[nodiscard]] void *address(const off_t offset) noexcept { return index(offset); }
-		[[nodiscard]] const void *address(const off_t offset) const noexcept { return index(offset); }
+		[[nodiscard]] void *address(const off_t offset) { return index(offset); }
+		[[nodiscard]] const void *address(const off_t offset) const { return index(offset); }
 
 		[[nodiscard]] bool sync(const int32_t flags = MS_SYNC | MS_INVALIDATE) const noexcept
 			{ return sync(_len, flags); }
@@ -119,6 +121,6 @@ namespace pcat
 	};
 
 	inline void swap(mmap_t &a, mmap_t &b) noexcept { a.swap(b); }
-}
+} // namespace pcat
 
 #endif /* MMAP__HXX */
