aafc01e96d50bc2a5ffb80db986fa0a9fcc7857a
dx-mon <dx-mon@users.sourceforge.net>
2020-06-15 11:37:02 +0100

    Moved the file chunking algorithm which is expressed through chunking_t and fileChunker_t, into a new header for unit testing

--- src/chunking.cxx
+++ src/chunking.cxx
@@ -1,81 +1,16 @@
-#include <cassert>
 #include <cerrno>
 #include <string_view>
 #include <substrate/console>
 #include "chunking.hxx"
 #include "mmap.hxx"
 #include "threadPool.hxx"
-#include "chunkState.hxx"
+#include "fileChunker.hxx"
 
 using namespace std::literals::string_view_literals;
 using substrate::console;
 
 namespace pcat
 {
-	struct chunking_t final
-	{
-	private:
-		inputFilesIterator_t file{inputFiles.begin()};
-		off_t inputLength{file->length()};
-		mappingOffset_t inputOffset{0, blockLength(inputLength)};
-		const off_t outputLength{outputFile.length()};
-		mappingOffset_t outputOffset{};
-
-		constexpr void nextInputBlock() noexcept
-		{
-			inputOffset += inputOffset.length();
-			if (inputOffset.offset() == inputLength)
-			{
-				assert(file < inputFiles.end()); // NOLINT
-				++file;
-				inputLength = file == inputFiles.end() ? 0 : file->length();
-				inputOffset = {};
-			}
-		};
-
-	public:
-		chunking_t() noexcept { outputOffset.length(blockLength(outputLength - outputOffset)); }
-		chunking_t(const inputFilesIterator_t file_) noexcept : file{file_}, inputLength{0}, inputOffset{},
-			outputOffset{outputLength} { }
-		[[nodiscard]] constexpr chunkState_t subchunkState() const noexcept
-			{ return {file, inputLength, inputOffset, outputOffset}; }
-		chunkState_t operator *() const noexcept { return subchunkState(); }
-
-		constexpr void operator ++() noexcept
-		{
-			if (outputOffset == outputLength)
-				return;
-			else if (outputOffset.length() != inputOffset.length())
-			{
-				const auto state{subchunkState().end()};
-				file = state.file();
-				inputLength = state.inputLength();
-				inputOffset = state.inputOffset();
-			}
-			nextInputBlock();
-			inputOffset.length(blockLength(inputLength - inputOffset));
-			outputOffset += outputOffset.length();
-			outputOffset.length(blockLength(outputLength - outputOffset));
-		}
-
-		bool operator ==(const chunking_t &other) const noexcept
-		{
-			return file == other.file &&
-				inputOffset == other.inputOffset &&
-				outputOffset == other.outputOffset;
-		}
-		bool operator !=(const chunking_t &other) const noexcept { return !(*this == other); }
-	};
-
-	// begin() and end() are non-static to fufill the requirements of for-each looping
-	struct fileChunker_t final
-	{
-		// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
-		chunking_t begin() noexcept { return {}; }
-		// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
-		chunking_t end() noexcept { return {inputFiles.end()}; }
-	};
-
 	int32_t copyChunk(chunkState_t chunk)
 	{
 		const auto &outputOffset = chunk.outputOffset();
---
+++ src/fileChunker.hxx
@@ -0,0 +1,73 @@
+#ifndef FILE_CHUNKER__HXX
+#define FILE_CHUNKER__HXX
+
+#include "chunkState.hxx"
+
+namespace pcat
+{
+	struct chunking_t final
+	{
+	private:
+		inputFilesIterator_t file{inputFiles.begin()};
+		off_t inputLength{file->length()};
+		mappingOffset_t inputOffset{0, blockLength(inputLength)};
+		const off_t outputLength{outputFile.length()};
+		mappingOffset_t outputOffset{};
+
+		constexpr void nextInputBlock() noexcept
+		{
+			inputOffset += inputOffset.length();
+			if (inputOffset.offset() == inputLength)
+			{
+				assert(file < inputFiles.end()); // NOLINT
+				++file;
+				inputLength = file == inputFiles.end() ? 0 : file->length();
+				inputOffset = {};
+			}
+		};
+
+	public:
+		chunking_t() noexcept { outputOffset.length(blockLength(outputLength - outputOffset)); }
+		chunking_t(const inputFilesIterator_t file_) noexcept : file{file_}, inputLength{0}, inputOffset{},
+			outputOffset{outputLength} { }
+		[[nodiscard]] constexpr chunkState_t subchunkState() const noexcept
+			{ return {file, inputLength, inputOffset, outputOffset}; }
+		chunkState_t operator *() const noexcept { return subchunkState(); }
+
+		constexpr void operator ++() noexcept
+		{
+			if (outputOffset == outputLength)
+				return;
+			else if (outputOffset.length() != inputOffset.length())
+			{
+				const auto state{subchunkState().end()};
+				file = state.file();
+				inputLength = state.inputLength();
+				inputOffset = state.inputOffset();
+			}
+			nextInputBlock();
+			inputOffset.length(blockLength(inputLength - inputOffset));
+			outputOffset += outputOffset.length();
+			outputOffset.length(blockLength(outputLength - outputOffset));
+		}
+
+		bool operator ==(const chunking_t &other) const noexcept
+		{
+			return file == other.file &&
+				inputOffset == other.inputOffset &&
+				outputOffset == other.outputOffset;
+		}
+		bool operator !=(const chunking_t &other) const noexcept { return !(*this == other); }
+	};
+
+	// begin() and end() are non-static to fufill the requirements of for-each looping
+	struct fileChunker_t final
+	{
+		// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+		chunking_t begin() noexcept { return {}; }
+		// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+		chunking_t end() noexcept { return {inputFiles.end()}; }
+	};
+} // namespace pcat
+
+#endif /*FILE_CHUNKER__HXX*/
