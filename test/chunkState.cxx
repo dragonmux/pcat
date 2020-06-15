9ed73c96e9c4a4646a2648822d3a5425fe4d13c7
dx-mon <dx-mon@users.sourceforge.net>
2020-06-15 16:49:47 +0100

    Implemented the second half of the unaligned chunk chunkState_t test

--- test/chunkState.cxx
+++ test/chunkState.cxx
@@ -105,6 +105,30 @@ namespace chunkState
 		suite.assertTrue(beginState->file() == endState.file());
 	}
 
+	void testFillSecondUnalignedChunk(testsuite &suite)
+	{
+		auto beginState{substrate::make_unique_nothrow<chunkState_t>
+		(
+			inputFiles.begin() + 2, transferBlockSize, mappingOffset_t{transferBlockSize - 4096, 4096},
+			mappingOffset_t{0, 4096}
+		)};
+		const chunkState_t endState
+		{
+			inputFiles.end(), 0, mappingOffset_t{}, mappingOffset_t{4096, 0}
+		};
+		suite.assertEqual(beginState->inputLength(), transferBlockSize);
+		suite.assertTrue(beginState->file() == inputFiles.begin() + 2);
+		suite.assertFalse(beginState->atEnd());
+		suite.assertTrue(beginState->end() == endState);
+		++*beginState;
+		suite.assertTrue(beginState->atEnd());
+		suite.assertTrue(*beginState == endState);
+		suite.assertTrue(beginState->inputLength() == endState.inputLength());
+		suite.assertTrue(beginState->inputOffset() == endState.inputOffset());
+		suite.assertTrue(beginState->outputOffset() == endState.outputOffset());
+		suite.assertTrue(beginState->file() == inputFiles.end());
+	}
+
 	void testFillUnalignedChunks(testsuite &suite)
 	{
 		suite.assertFalse(inputFiles.begin() == inputFiles.end());
@@ -113,5 +137,6 @@ namespace chunkState
 		suite.assertEqual(inputFiles[1].length(), 3072);
 		suite.assertEqual(inputFiles[2].length(), transferBlockSize);
 		testFillFirstUnalignedChunk(suite);
+		testFillSecondUnalignedChunk(suite);
 	}
 } // namespace chunkState
