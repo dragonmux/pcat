d4756f811a659f9eda264fe7a72bafad674abfa8
dx-mon <dx-mon@users.sourceforge.net>
2020-06-22 07:43:19 +0100

    Seems that testThreadPool's testQueueWait was unable to be reliable now on the CI platform, so changed the MO of the test

--- test/threadPool.cxx
+++ test/threadPool.cxx
@@ -60,8 +60,7 @@ namespace threadPool
 			suite.assertFalse(pool.queue(threads - i));
 		suite.assertFalse(pool.ready());
 		suite.assertFalse(pool.queue(threads));
-		std::this_thread::sleep_for(50ms);
-		suite.assertTrue(pool.queue(threads));
+		suite.assertFalse(pool.queue(threads));
 		suite.assertTrue(pool.finish());
 		suite.assertFalse(pool.valid());
 		suite.assertFalse(pool.finish());
