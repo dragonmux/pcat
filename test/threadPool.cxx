82c724fa772429974ac7753489a82833a8930d3d
dx-mon <dx-mon@users.sourceforge.net>
2020-06-22 07:32:44 +0100

    Altered the queue wait test for threadPool_t to cater for the internals change from a single-slot queue system to a multi-slot system

--- test/threadPool.cxx
+++ test/threadPool.cxx
@@ -60,6 +60,7 @@ namespace threadPool
 			suite.assertFalse(pool.queue(threads - i));
 		suite.assertFalse(pool.ready());
 		suite.assertFalse(pool.queue(threads));
+		std::this_thread::sleep_for(30ms);
 		suite.assertTrue(pool.queue(threads));
 		suite.assertTrue(pool.finish());
 		suite.assertFalse(pool.valid());
