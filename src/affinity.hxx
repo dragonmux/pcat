031870cb15655cb6ac09c284ea4664b91b85228d
dx-mon <dx-mon@users.sourceforge.net>
2020-06-29 11:34:44 +0100

    Simplified affinity_t::numProcessors() back down now the logic to cap on number of threads is actually in the constructor

--- src/affinity.hxx
+++ src/affinity.hxx
@@ -45,14 +45,7 @@ namespace pcat
 			}
 		}
 
-		[[nodiscard]] auto numProcessors() const noexcept
-		{
-			const auto *const threadCount{dynamic_cast<args::argThreads_t *>(::args->find(argType_t::threads))};
-			if (threadCount)
-				return std::min(processors.size(), threadCount->threads());
-			return processors.size();
-		}
-
+		[[nodiscard]] auto numProcessors() const noexcept { return processors.size(); }
 		[[nodiscard]] auto begin() const noexcept { return processors.begin(); }
 		[[nodiscard]] auto end() const noexcept { return processors.end(); }
 
