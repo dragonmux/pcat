a0c85d6965e8cc5a519818b0dc0c67cd4075905b
dx-mon <dx-mon@users.sourceforge.net>
2020-07-20 18:13:27 +0100

    Implemented the main chunking algorithm for chunkSpans to provide the proper input to the compute threads

--- src/algorithm/chunkSpans/chunking.cxx
+++ src/algorithm/chunkSpans/chunking.cxx
@@ -19,6 +19,18 @@ namespace pcat::algorithm::chunkSpans
 		mappingOffset_t inputOffset_{};
 		mappingOffset_t outputOffset_{};
 
+		constexpr void nextInputBlock() noexcept
+		{
+			inputOffset_ += inputOffset_.length();
+			if (inputOffset_.offset() == inputLength_)
+			{
+				assert(file_ != inputFiles.end()); // NOLINT
+				++file_;
+				inputLength_ = file_ == inputFiles.end() ? 0 : file_->length();
+				inputOffset_ = {};
+			}
+		};
+
 	public:
 		chunkState_t() noexcept = default;
 		constexpr chunkState_t(const inputFilesIterator_t &file, const off_t inputLength,
@@ -38,20 +50,38 @@ namespace pcat::algorithm::chunkSpans
 		[[nodiscard]] constexpr chunkState_t end() const noexcept
 		{
 			chunkState_t state{*this};
-			/*while (!state.atEnd())
-				++state;*/
+			while (!state.atEnd())
+				++state;
 			return state;
+		}
+
+		[[nodiscard]] constexpr bool atEnd() const noexcept
+			{ return !outputOffset_.length(); }
+
+		constexpr void operator ++() noexcept
+		{
+			if (atEnd())
+				return;
+			const off_t remainder = outputOffset_.length() - inputOffset_.length();
+			outputOffset_ += inputOffset_.length();
+			outputOffset_.length(remainder);
+			nextInputBlock();
+			inputOffset_.length(std::min(transferBlockSize, std::min(remainder, inputLength_)));
 		}
 	};
 
 	struct chunking_t final
 	{
 	private:
+		std::size_t spanLength;
 		inputFilesIterator_t file{inputFiles.begin()};
 		off_t inputLength{file == inputFiles.end() ? 0 : file->length()};
 		mappingOffset_t inputOffset{0, blockLength(inputLength)};
 		const off_t outputLength{outputFile.length()};
 		mappingOffset_t outputOffset{};
+
+		constexpr off_t spanOf(const off_t length)
+			{ return std::min(off_t(spanLength), length); }
 
 		constexpr void nextInputBlock() noexcept
 		{
@@ -63,13 +93,14 @@ namespace pcat::algorithm::chunkSpans
 				inputLength = file == inputFiles.end() ? 0 : file->length();
 				inputOffset = {};
 			}
+			inputOffset.length(blockLength(inputLength - inputOffset));
 		};
 
 	public:
-		chunking_t(const std::size_t spanLength) noexcept
-			{ outputOffset.length(blockLength(outputLength - outputOffset)); }
-		chunking_t(const std::size_t spanLength, const inputFilesIterator_t file_) noexcept :
-			file{file_}, outputOffset{outputLength} { }
+		chunking_t(const std::size_t spanLength_) noexcept : spanLength{spanLength_}
+			{ outputOffset.length(spanOf(outputLength - outputOffset)); }
+		chunking_t(const std::size_t spanLength_, const inputFilesIterator_t file_) noexcept :
+			spanLength{spanLength_}, file{file_}, outputOffset{outputLength} { }
 		[[nodiscard]] constexpr chunkState_t subchunkState() const noexcept
 			{ return {file, inputLength, inputOffset, outputOffset}; }
 		chunkState_t operator *() const noexcept { return subchunkState(); }
@@ -85,10 +116,15 @@ namespace pcat::algorithm::chunkSpans
 				inputLength = state.inputLength();
 				inputOffset = state.inputOffset();
 			}
-			nextInputBlock();
-			inputOffset.length(blockLength(inputLength - inputOffset));
+			if (file != inputFiles.end())
+				nextInputBlock();
 			outputOffset += outputOffset.length();
-			outputOffset.length(blockLength(outputLength - outputOffset));
+			if (outputOffset == outputLength)
+				outputOffset.length(0);
+			else if (outputLength - outputOffset - spanLength < spanLength)
+				outputOffset.length(outputLength - outputOffset);
+			else
+				outputOffset.length(spanLength);
 		}
 
 		bool operator ==(const chunking_t &other) const noexcept
@@ -151,15 +187,18 @@ namespace pcat::algorithm::chunkSpans
 
 		const auto chunksPerSpan{outputFile.length() / (transferBlockSize * copyThreads.numProcessors())};
 		fileChunker_t chunker{chunksPerSpan * transferBlockSize};
+		const auto spanLength{chunksPerSpan * transferBlockSize};
 
 		console.info("Copying "sv, outputFile.length(), " bytes using "sv,
 			copyThreads.numProcessors(), " threads"sv);
-
 		console.info("Each thread must process "sv, chunksPerSpan, " chunks ("sv, chunker.spanLength(),
-			" bytes) + "sv, outputFile.length() - (chunksPerSpan * transferBlockSize *
-			copyThreads.numProcessors()), " additional bytes on the final span"sv);
+			" bytes) + "sv, outputFile.length() - (spanLength * copyThreads.numProcessors()),
+			" additional bytes on the final span"sv);
 
-		return 0;
+		const auto endState{*chunker.end()};
+		console.info("End state is "sv, endState.inputLength(), " bytes at ", endState.inputOffset().offset(),
+			" with output region of ", endState.outputOffset().length(), " bytes at ",
+			endState.outputOffset().offset(), " using file ", int32_t{endState.inputFile()});
 
 		for (const chunkState_t &chunk : chunker)
 		{
@@ -169,8 +208,16 @@ namespace pcat::algorithm::chunkSpans
 				console.error("Copying failed: "sv, std::strerror(result));
 				return result;
 			}*/
-			if (auto result{copyChunk(chunk)}; !result)
-				return result;
+			console.info("Copying "sv, chunk.inputLength(), " bytes at ", chunk.inputOffset().offset(),
+				" to output region of ", chunk.outputOffset().length(), " bytes at ", chunk.outputOffset().offset(),
+				" using file ", int32_t{chunk.inputFile()});
+			/*if (auto result{copyChunk(chunk)}; !result)
+				return result;*/
+			if (!chunk.inputLength())
+			{
+				console.error("Algorithmic failure, impossible input state");
+				break;
+			}
 		}
 		//return copyThreads.finish();
 		return 0;
