3666cb718cf1bdba0c027fd09f14470852e652d7
dx-mon <dx-mon@users.sourceforge.net>
2020-08-03 14:48:25 +0100

    Implemented a windows-specific version of affinity_t

--- src/affinity.hxx
+++ src/affinity.hxx
@@ -6,13 +6,20 @@
 #include <algorithm>
 #include <system_error>
 #include <stdexcept>
-#include <sched.h>
+#ifndef _WINDOWS
+#	include <sched.h>
+#else
+#	include <utility>
+#	include <winbase.h>
+#	include <substrate/fixed_vector>
+#endif
 #include "args.hxx"
 #include "indexSequence.hxx"
 
 namespace pcat
 {
-	struct affinity_t
+#ifndef _WINDOWS
+	struct affinity_t final
 	{
 	private:
 		std::vector<uint32_t> processors;
@@ -57,6 +64,77 @@ namespace pcat
 			{ pinTo(thread.native_handle(), index); }
 		void pinThreadTo(const uint64_t index) const { pinTo(pthread_self(), index); }
 	};
+#else
+	using substrate::fixedVector_t;
+	using ulong_t = unsigned long;
+
+	struct affinity_t final
+	{
+	private:
+		std::vector<std::pair<uint16_t, uint8_t>> processors;
+
+		void pinTo(const HANDLE thread, const uint32_t index) const
+		{
+			if (index >= processors.size())
+				throw std::out_of_range{"index into thread affinity object too large"};
+			const auto &[group, processor] = processors[index];
+			const auto groupMask = UINT64_C(1) << processor;
+			const GROUP_AFFINITY affinity{groupMask, group, {}};
+			SetThreadGroupAffinity(thread, &affinity, nullptr);
+		}
+
+	public:
+		affinity_t() : processors{}
+		{
+			fixedVector_t<SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX> processorInfo{};
+			ulong_t returnLength{};
+
+			while (!GetLogicalProcessorInformationEx(RelationGroup, processorInfo.data(), &returnLength))
+			{
+				const auto error{GetLastError()};
+				if (error == ERROR_INSUFFICIENT_BUFFER)
+				{
+					constexpr auto structLength{sizeof(SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX)};
+					// Compute how many structs worth of information the kernel has to return..
+					auto count{returnLength / structLength};
+					count += returnLength - (count * structLength) ? 1 : 0;
+					// Then allocate a fixedVector_t large enough for that.
+					processorInfo = {count};
+				}
+				else
+					throw std::system_error{error, std::system_category()};
+			}
+
+			for (const auto &processor : processorInfo)
+			{
+				if (processor.Relationship != RelationGroup)
+					continue;
+				for (uint16_t groupIndex{}; groupIndex < processor.Group.ActiveGroupCount; ++groupIndex)
+				{
+					const auto &group{processor.Group.GroupInfo[groupIndex]};
+					auto mask{group.ActiveProcessorMask};
+					for (uint8_t i{}; i < sizeof(KAFFINITY) * 8; ++i)
+					{
+						if (mask & 1)
+							processors.emplace_back(groupIndex, i);
+						mask >>= 1;
+						if (!mask)
+							break;
+					}
+				}
+			}
+		}
+
+		[[nodiscard]] auto numProcessors() const noexcept { return processors.size(); }
+		[[nodiscard]] auto begin() const noexcept { return processors.begin(); }
+		[[nodiscard]] auto end() const noexcept { return processors.end(); }
+		[[nodiscard]] auto indexSequence() const noexcept { return indexSequence_t{0, numProcessors()}; }
+
+		void pinThreadTo(std::thread &thread, const uint32_t index) const
+			{ pinTo(thread.native_handle(), index); }
+		void pinThreadTo(const uint64_t index) const { pinTo(GetCurrentThread(), index); }
+	};
+#endif /*_WINDOWS*/
 } // namespace pcat
 
 #endif /*AFFINITY__HXX*/
